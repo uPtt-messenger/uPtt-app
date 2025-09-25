067d15b517abb02dd10449084d20158047cbb271
CodingMan <pttcodingman@gmail.com>
2025-09-25 09:34:38 +0800

    chore: adjust init.py and setup.py.

--- src/uPttTerm/__init__.py
+++ src/uPttTerm/__init__.py
@@ -1,4 +1,9 @@
 
+
 __name__ = "uPttTerm"
 __version__ = "0.1.0"
 __author__ = "CodingMan"
+__author_email__ = "pttcodingman@gmail.com"
+__license__ = "GPL-3.0-only"
+__url__ = "https://github.com/uPtt-messenger/uPttTerm"
+__description__ = "A TUI application for real-time chat on PTT via its built-in private message system."
